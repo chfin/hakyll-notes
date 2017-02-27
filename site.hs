64f2f793a494f06136727656b72c311d73075d96
Christoph Finkensiep <chfin@freenet.de>
2017-02-27 16:51:45 +0100

    include bib notes in bibliography

--- site.hs
+++ site.hs
@@ -4,6 +4,12 @@ import           Data.Monoid (mappend)
 import           Data.Foldable (concatMap)
 import           Hakyll as H
 import           Control.Monad
+
+import System.FilePath (replaceExtension)
+import Data.List.Split
+import Data.List (find)
+import Data.Either (rights)
+
 import Text.CSL.Input.Bibtex (readBibtex)
 import qualified Text.CSL.Reference as Ref
 import Text.CSL.Parser (readCSLFile)
@@ -12,12 +18,12 @@ import Text.CSL.Output.Pandoc (renderPandoc')
 import Text.CSL.Style ( Locale(..), styleDefaultLocale, styleLocale
                       , BiblioData(..), Style, Formatted
                       , citeId, emptyCite)
-import System.FilePath (replaceExtension)
 
 import Text.Pandoc.Definition
 import Text.Pandoc.Builder as B
 import Text.Pandoc.Walk (walk)
 import Text.Pandoc.Options
+import Text.Pandoc (readOrg)
 
 --------------------------------------------------------------------------------
 bibFile :: FilePath
@@ -27,24 +33,27 @@ cslFile :: FilePath
 cslFile = "csl/chicago-author-date.csl"
 
 paperNotesFile :: FilePath
-paperNotesFile = "paper_notes.org"
+paperNotesFile = (replaceExtension bibFile "org")
 
 main :: IO ()
 main = hakyll $ do
     match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+      route   idRoute
+      compile compressCssCompiler
+    match "js/*" $ do
+      route   idRoute
+      compile copyFileCompiler
 
-    match "notes/*.bib" $ version "bib" $ compile biblioCompiler
+    match "notes/**.bib" $ version "bib" $ compile biblioCompiler
     match "csl/*.csl" $ compile cslCompiler
 
-    match "notes/*.bib" $ version "html" $ do
-      route $ setExtension "html"
+    match "notes/**.bib" $ version "html" $ do
+      route $ setExtension "bib.html"
       compile $ compileBib cslFile
         >>= loadAndApplyTemplate "templates/default.html" defaultContext
         >>= relativizeUrls
 
-    match "notes/*.org" $ do
+    match "notes/**.org" $ do
       route $ setExtension "html"
       compile $ do
         let noteCtx = constField "isNote" "true" `mappend` defaultContext
@@ -53,20 +62,20 @@ main = hakyll $ do
           >>= relativizeUrls
         
     match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- loadAll "notes/*.org"
-            bibs <- loadAll ("notes/*.bib" .&&. hasVersion "html")
-            let indexCtx =
-                    listField "posts" defaultContext (return posts) `mappend`
-                    listField "bibs" defaultContext (return bibs) `mappend`
-                    constField "title" "Index" `mappend`
-                    defaultContext
-
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
+      route idRoute
+      compile $ do
+        posts <- loadAll "notes/*.org"
+        bibs <- loadAll ("notes/*.bib" .&&. hasVersion "html")
+        let indexCtx =
+              listField "posts" defaultContext (return posts) `mappend`
+              listField "bibs" defaultContext (return bibs) `mappend`
+              constField "title" "Index" `mappend`
+              defaultContext
+        
+        getResourceBody
+          >>= applyAsTemplate indexCtx
+          >>= loadAndApplyTemplate "templates/default.html" indexCtx
+          >>= relativizeUrls
 
     match "templates/*" $ compile templateBodyCompiler
 
@@ -82,26 +91,58 @@ processBib s rs = citeproc procOpts s rs [map (\r -> emptyCite { citeId = Ref.un
 
 paperNotesLink = replaceExtension paperNotesFile "html"
 
-renderRef :: Style -> (Formatted, String) -> [Block]
-renderRef s (ref, id) = toList $ extend block
-  where block = renderPandoc' s (ref, id)
-        extend (Div a bs) = divWith a ((B.fromList bs) <> linkPara)
-        extend blk = divWith nullAttr (singleton blk) <> linkPara
-        linkPara = divWith pAttr (para $ link noteLink "Go to paper notes" "Notes")
-        noteLink = paperNotesLink ++ "#" ++ id
-        pAttr = ("", ["reference-links"], [])
+matchNoteEntry :: String -> [Block] -> Bool
+matchNoteEntry ref ((Header _ (id, _, _) _):_)
+  | ref == id = True
+matchNoteEntry _ _ = False
+
+classAttr :: String -> Attr
+classAttr c = ("", [c], [])
+
+renderRef :: Style -> [[Block]] -> (Formatted, String) -> [Block]
+renderRef style nEntries (ref, rId) = toList $ extend block
+  where block = renderPandoc' style (ref, rId)
+        extend (Div a bs) = divWith a ((B.fromList bs) <> linkPara <> noteDiv)
+        extend blk = divWith nullAttr (singleton blk) <> linkPara <> noteDiv
+        linkPara = if null nEntry then
+                     mempty
+                   else 
+                     divWith pAttr (para $ showNotes <> space <> noteLink)
+        noteLink = link ("/" ++ paperNotesLink ++ "#" ++ rId) "Go to paper notes" "go to notes →"
+        showNotes = singleton $
+          Link (classAttr "reference-toggle-notes") [Str "show / hide notes"] ("#","")
+        pAttr = classAttr "reference-links"
+        nEntry = maybe [] tail (find (matchNoteEntry rId) nEntries)
+        noteDiv = singleton $ Div nAttr nEntry
+        nAttr = classAttr "reference-notes"
+
+isH2 :: Block -> Bool
+isH2 (Header 2 _ _) = True
+isH2 _              = False
+
+splitNotes :: Pandoc -> [[Block]]
+splitNotes notes = (split . dropInitBlank . keepDelimsL .whenElt)
+                   isH2 (blocks notes)
+  where blocks (Pandoc _ b) = b
 
 compileBib :: FilePath -> Compiler (Item String)
-compileBib stylefn = do
+compileBib styleFp = do
   -- bibI <- load (fromFilePath bibfn)
-  bibfn <- getUnderlying
-  style <- unsafeCompiler $ readCSLFile Nothing stylefn
-  refs <- unsafeCompiler $ readBibtex False True (toFilePath bibfn)
-  let bibData = processBib style refs
-      bib     = bibliography bibData
-      blocks  = (zip bib (citationIds bibData)) >>= (renderRef style)
-      res     = doc (B.fromList blocks)
-  return $ writePandoc (Item bibfn res)
+  bibFp <- getUnderlying
+  style <- unsafeCompiler $ readCSLFile Nothing styleFp
+  refs  <- unsafeCompiler $ readBibtex False True (toFilePath bibFp)
+  bibI  <- load $ setVersion (Just "bib") bibFp
+  cslI  <- load $ fromFilePath styleFp
+  notes <- (unsafeCompiler $ readFile paperNotesFile)
+           >>= return . Item (fromFilePath paperNotesFile)
+           >>= readPandocBiblio defaultHakyllReaderOptions cslI bibI
+           >>= return . walk (linkCitations bibI)
+  let bibData  = processBib style refs
+      bib      = bibliography bibData
+      nEntries = splitNotes (itemBody notes)
+      blocks   = (zip bib (citationIds bibData)) >>= (renderRef style nEntries)
+      res      = doc (B.fromList blocks)
+  return $ writePandoc (Item bibFp res)
 
 asideTemplate = "<aside id=\"toc\" class=\"container\"><div>$toc$</div></aside>\
                 \<main class=\"container\">\
@@ -128,7 +169,7 @@ linkCitations :: Item Biblio -> Inline -> Inline
 linkCitations bib (Cite cts txts) = Span nullAttr (zipWith mkLink cts txts) 
   where mkLink c t = Link ("",["citation-link"],[]) [Cite [c] [t]] (mkTarget c)
         mkTarget c = ("/" ++ path ++ "#ref-" ++ citationId c, "")
-        path       = replaceExtension (toFilePath (itemIdentifier bib)) "html"
+        path       = replaceExtension (toFilePath (itemIdentifier bib)) "bib.html"
 linkCitations _ i = i
 
 incHeader :: Block -> Block
